f457b6ff601d5c6e91985d83d59fe7a6f13b6459
omar morales muñoz <omar_mm_12@hotmail.com>
2019-06-24 05:06:31 -0500

    version beta de los contactos con ajaxs

--- SpotiFake/Controllers/ContactoController.cs
+++ SpotiFake/Controllers/ContactoController.cs
@@ -21,8 +21,25 @@ namespace SpotiFake.Controllers
         }
         public ActionResult getUsers(int idUsuario)
         {
+            List<DetalleUsuario> contactos = spotiFakeContext.DetalleUsuarios.Where(o => o.idUsuario == idUsuario).ToList();
+            List<Usuario> listContactos = new List<Usuario>();
+            foreach (var contacto in contactos)
+            {
+                var iterar=spotiFakeContext.Usuarios.Where(o => o.idUsuario == contacto.idContacto).FirstOrDefault();
+                if (iterar!=null)
+                {
+                    listContactos.Add(iterar);
+                }
+                
+            }
+            
             var usuario = spotiFakeContext.Usuarios.Where(o => o.rol == "Usuario" && o.idUsuario != idUsuario).ToList();
             return View(usuario);
+        }
+        public ActionResult getUsersAmigos(int idUsuario)
+        {
+            var amigos = spotiFakeContext.DetalleUsuarios.Where(o => o.idUsuario == idUsuario).Include(o=>o.usuario).ToList();
+            return View(amigos);
         }
         public ActionResult AgregarAmigo(int idUsuario, int idContacto)
         {
@@ -36,16 +53,19 @@ namespace SpotiFake.Controllers
                 spotiFakeContext.DetalleUsuarios.Add(detalleUsuario);
                 spotiFakeContext.SaveChanges();
 
-                detalleUsuario.idUsuario = idContacto;
-                detalleUsuario.idContacto = idUsuario;
-
-                spotiFakeContext.DetalleUsuarios.Add(detalleUsuario);
-                spotiFakeContext.SaveChanges();
             }
             
 
             var usuario = spotiFakeContext.Usuarios.Where(o => o.rol == "Usuario"&&o.idUsuario!=idUsuario).ToList();
             return View("Index", usuario);
+        }
+        public ActionResult dejarAmigo(int idUsuario,int idContacto)
+        {
+            var amigo = spotiFakeContext.DetalleUsuarios.Where(o => o.idUsuario == idUsuario && o.idContacto == idContacto).FirstOrDefault();
+            spotiFakeContext.DetalleUsuarios.Remove(amigo);
+            spotiFakeContext.SaveChanges();
+            var amigos = spotiFakeContext.DetalleUsuarios.Where(o => o.idUsuario == idUsuario).Include(o => o.usuario).ToList();
+            return View("getUsersAmigos", amigos);
         }
 
         public ActionResult Amigos(int idUsuario)
