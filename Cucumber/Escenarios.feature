3b358c6bf0a2e458041f4a2e91333a22097c8df7
omar morales muñoz <omar_mm_12@hotmail.com>
2019-06-24 22:38:57 -0500

    actualizando solucion cucumber

--- Cucumber/Escenarios.feature
+++ Cucumber/Escenarios.feature
@@ -1,8 +1,8 @@
 ﻿Feature: Escenarios
 
 @mytag
-Scenario: Testeando Login para el usuario
-	Given el correo del usario
-	And la contraseña
+Scenario: Testeando Login
+	Given el correo del usario usuario1@correo.com
+	And la contraseña 123
 	When el usaurio quiera iniciar session
-	Then la interfaz de la web cambiara a la pagina inicio del usuario: 
+	Then la pagina web redirecionara a vista del usuario con nombre: usuario1
---
+++ Cucumber/testLogin.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace Cucumber
+{
+    [Binding]
+    public class EscenariosSteps
+    {
+        ChromeDriver driver = new ChromeDriver(@"c:");
+        [Given(@"el correo del usario (.*)")]
+        public void GivenElCorreoDelUsario(string correoUser)
+        {
+            driver.Url = "http://localhost:57748/";
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            var inputCorreo = driver.FindElementById("user");
+            inputCorreo.Clear();
+            inputCorreo.SendKeys(correoUser);
+        }
+        
+        [Given(@"la contraseña (.*)")]
+        public void GivenLaContrasena(string passUser)
+        {
+            var inputPass = driver.FindElementById("pass");
+            inputPass.Clear();
+            inputPass.SendKeys(passUser);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+        
+        [When(@"el usaurio quiera iniciar session")]
+        public void WhenElUsaurioQuieraIniciarSession()
+        {
+            var btnIniciarSession = driver.FindElementById("btnIniciarSession");
+            btnIniciarSession.Click();
+        }
+        
+        [Then(@"la pagina web redirecionara a vista del usuario con nombre: (.*)")]
+        public void ThenLaInterfazDeLaWebCambiaraALaPaginaInicioDelUsuario(string userName)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            var saludoUser = driver.FindElementById("saludoAlUsuario");
+            Assert.AreEqual(saludoUser.Text, "Bienvenido "+userName);
+            driver.Close();
+        }
+    }
+}
